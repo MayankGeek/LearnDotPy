d0f15bb0ca343fa007a79fa94f4e768b40cfe347
Mayank Sharma <55140492+MayankGeek@users.noreply.github.com>
2022-12-13 23:43:39 +0530

    added editor route

--- LearnDotPy/home/views.py
+++ LearnDotPy/home/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html',)
+    return render(request,'home.html')
+
+def codeplayground(request):
+    return HttpResponse("<h1>under development</h1>")
+    
